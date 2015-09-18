d3b01dfc99fa160308f58d659a3988c1b59d7f88
Jean Giard <jgiard@antidot.net>
2015-09-18 09:33:58 +0200

    add tests for camel_case_name

---
+++ pyckson/test/test_helpers.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from pyckson.helpers import camel_case_name
+
+
+class CamelCaseNameTest(TestCase):
+    def test_should_do_nothing_on_simple_names(self):
+        self.assertEqual(camel_case_name('foo'), 'foo')
+
+    def test_should_camel_case_when_there_is_an_underscore(self):
+        self.assertEqual(camel_case_name('foo_bar'), 'fooBar')
