e2836fd8eb0d84d703ef171158775a447ccdf542
Jean Giard <jgiard@antidot.net>
2015-09-22 15:56:27 +0200

    add setup.py and change license

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='Pyckson',
+      version='0.1',
+      description='A minimalist python object mapper',
+      long_description='Pyckson is python library that allows you to simply convert python object to and from json',
+      url='https://github.com/antidot/Pyckson',
+      author='Jean Giard',
+      author_email='opensource@antidot.net',
+      license='LGPL',
+      classifier=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+          'Programming Language :: Python :: 3'
+      ],
+      keywords='pyckson json',
+      packages=find_packages(exclude=['pyckson.test']),
+      install_requires=[],
+      test_suite='pyckson.test')
