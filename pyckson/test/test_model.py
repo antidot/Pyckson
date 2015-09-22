0eabc21c4211ab73833c82802c6a834c7d5c0d4d
Jean Giard <jgiard@antidot.net>
2015-09-22 16:06:37 +0200

    fix model tests

--- pyckson/test/test_model.py
+++ pyckson/test/test_model.py
@@ -1,6 +1,7 @@
 from inspect import Parameter, Signature
 from unittest import TestCase
 
+from pyckson.builders import build_pyckson_attribute, build_pyckson_model
 from pyckson.model import PycksonAttribute, ListType, PycksonModel
 
 
@@ -8,7 +9,7 @@ class PycksonAttributeTest(TestCase):
     def test_should_build_simple_parameter(self):
         parameter = Parameter('foo', Parameter.POSITIONAL_OR_KEYWORD, annotation=int)
 
-        attribute = PycksonAttribute.from_parameter(parameter)
+        attribute = build_pyckson_attribute(parameter)
 
         self.assertEqual(attribute.python_name, 'foo')
         self.assertEqual(attribute.attr_type, int)
@@ -17,7 +18,7 @@ class PycksonAttributeTest(TestCase):
     def test_should_build_optional_parameter(self):
         parameter = Parameter('foo', Parameter.POSITIONAL_OR_KEYWORD, annotation=int, default=None)
 
-        attribute = PycksonAttribute.from_parameter(parameter)
+        attribute = build_pyckson_attribute(parameter)
 
         self.assertEqual(attribute.python_name, 'foo')
         self.assertEqual(attribute.attr_type, int)
@@ -26,7 +27,7 @@ class PycksonAttributeTest(TestCase):
     def test_should_consider_other_default_values_as_mandatory(self):
         parameter = Parameter('foo', Parameter.POSITIONAL_OR_KEYWORD, annotation=int, default=0)
 
-        attribute = PycksonAttribute.from_parameter(parameter)
+        attribute = build_pyckson_attribute(parameter)
 
         self.assertEqual(attribute.python_name, 'foo')
         self.assertEqual(attribute.attr_type, int)
@@ -35,7 +36,7 @@ class PycksonAttributeTest(TestCase):
     def test_should_camel_case_name(self):
         parameter = Parameter('foo_bar', Parameter.POSITIONAL_OR_KEYWORD, annotation=int)
 
-        attribute = PycksonAttribute.from_parameter(parameter)
+        attribute = build_pyckson_attribute(parameter)
 
         self.assertEqual(attribute.python_name, 'foo_bar')
         self.assertEqual(attribute.json_name, 'fooBar')
@@ -44,42 +45,42 @@ class PycksonAttributeTest(TestCase):
         parameter = Parameter('foo', Parameter.POSITIONAL_OR_KEYWORD)
 
         with self.assertRaises(TypeError):
-            PycksonAttribute.from_parameter(parameter)
+            build_pyckson_attribute(parameter)
 
     def test_should_not_accept_keyword_only_parameter(self):
         parameter = Parameter('foo', Parameter.KEYWORD_ONLY, annotation=int)
 
         with self.assertRaises(TypeError):
-            PycksonAttribute.from_parameter(parameter)
+            build_pyckson_attribute(parameter)
 
     def test_should_not_accept_positional_only_parameter(self):
         parameter = Parameter('foo', Parameter.POSITIONAL_ONLY, annotation=int)
 
         with self.assertRaises(TypeError):
-            PycksonAttribute.from_parameter(parameter)
+            build_pyckson_attribute(parameter)
 
     def test_should_not_accept_var_positionalparameter(self):
         parameter = Parameter('foo', Parameter.VAR_POSITIONAL, annotation=int)
 
         with self.assertRaises(TypeError):
-            PycksonAttribute.from_parameter(parameter)
+            build_pyckson_attribute(parameter)
 
     def test_should_not_accept_var_keyword_parameter(self):
         parameter = Parameter('foo', Parameter.VAR_KEYWORD, annotation=int)
 
         with self.assertRaises(TypeError):
-            PycksonAttribute.from_parameter(parameter)
+            build_pyckson_attribute(parameter)
 
     def test_should_refuse_unspecified_list(self):
         parameter = Parameter('foo', Parameter.POSITIONAL_OR_KEYWORD, annotation=list)
 
         with self.assertRaises(TypeError):
-            PycksonAttribute.from_parameter(parameter)
+            build_pyckson_attribute(parameter)
 
     def test_should_accept_specified_list(self):
         parameter = Parameter('foo', Parameter.POSITIONAL_OR_KEYWORD, annotation=list)
 
-        attribute = PycksonAttribute.from_parameter(parameter, sub_type=int)
+        attribute = build_pyckson_attribute(parameter, sub_type=int)
 
         self.assertEqual(type(attribute.attr_type), ListType)
         self.assertEqual(attribute.attr_type.sub_type, int)
@@ -91,7 +92,7 @@ class PycksonModelTest(TestCase):
         param2 = Parameter('bar', Parameter.POSITIONAL_OR_KEYWORD, annotation=str)
         signature = Signature([param1, param2])
 
-        model = PycksonModel.from_signature(signature, {})
+        model = build_pyckson_model(signature, {})
 
         self.assertEqual(model.get_attribute(python_name='foo').python_name, 'foo')
         self.assertEqual(model.get_attribute(python_name='bar').python_name, 'bar')
@@ -101,7 +102,7 @@ class PycksonModelTest(TestCase):
         param2 = Parameter('self', Parameter.POSITIONAL_OR_KEYWORD)
         signature = Signature([param1, param2])
 
-        model = PycksonModel.from_signature(signature, {})
+        model = build_pyckson_model(signature, {})
 
         self.assertEqual(model.get_attribute(python_name='foo').python_name, 'foo')
         with self.assertRaises(KeyError):
@@ -112,7 +113,7 @@ class PycksonModelTest(TestCase):
         param2 = Parameter('bar', Parameter.POSITIONAL_OR_KEYWORD, annotation=list)
         signature = Signature([param1, param2])
 
-        model = PycksonModel.from_signature(signature, {'foo': int, 'bar': list})
+        model = build_pyckson_model(signature, {'foo': int, 'bar': list})
 
         self.assertEqual(model.get_attribute(python_name='foo').python_name, 'foo')
         with self.assertRaises(KeyError):
