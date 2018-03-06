33334b6b3ac0f3e61a3068f6da5e003064701884
Jean Giard <jgiard@antidot.net>
2018-03-06 10:31:14 +0100

    dirty fix for Union type check, until I find a better way

--- src/pyckson/model/builder.py
+++ src/pyckson/model/builder.py
@@ -32,8 +32,26 @@ class PycksonModelBuilder:
         return PycksonModel(attributes)
 
     def is_optional_type(self, param_type):
-        return issubclass(param_type, Union) and len(param_type.__union_params__) == 2 and \
-               isinstance(None, param_type.__union_params__[1])
+        # seems like at some point internal behavior on typing Union changed
+        # https://bugs.launchpad.net/ubuntu/+source/python3.5/+bug/1650202
+        if "Union" not in str(param_type):
+            return False
+        if hasattr(param_type, '__origin__'):
+            is_union = param_type.__origin__ == Union
+        else:
+            is_union = issubclass(param_type, Union)
+
+        if not is_union:
+            return False
+
+        union_args = self.get_union_params(param_type)
+        return is_union and len(union_args) == 2 and isinstance(None, union_args[1])
+
+    def get_union_params(self, param_type):
+        if hasattr(param_type, '__args__'):
+            return param_type.__args__
+        else:
+            return param_type.__union_params__
 
     def build_attribute(self, parameter: Parameter) -> PycksonAttribute:
         python_name = parameter.name
@@ -44,7 +62,7 @@ class PycksonModelBuilder:
         if parameter.kind != Parameter.POSITIONAL_OR_KEYWORD:
             raise TypeError('pyckson only handle named parameters')
         if self.is_optional_type(parameter.annotation):
-            return PycksonAttribute(python_name, json_name, parameter.annotation.__union_params__[0], True,
+            return PycksonAttribute(python_name, json_name, self.get_union_params(parameter.annotation)[0], True,
                                     self.serializer_provider.get(str, self.cls, python_name),
                                     self.parser_provider.get(str, self.cls, python_name))
 
