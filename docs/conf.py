fa8162d4778e19ff8003be63a73452ad2f6e9386
Jean Giard <jgiard@antidot.net>
2018-03-08 11:12:34 +0100

    Fix doc build

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 from pyckson import __version__
 
 # -- General configuration ------------------------------------------------
