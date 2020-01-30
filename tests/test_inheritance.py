0b511114276b045a078e151855422e11c5511a5e
Jean Giard <jgiard@antidot.net>
2020-01-30 11:08:52 +0100

    Add another inheritance test

--- tests/test_inheritance.py
+++ tests/test_inheritance.py
@@ -19,3 +19,21 @@ def test_bug_14():
 
     assert pyckson.loads(Parent, x) == Parent('toto', 'plip')
     assert pyckson.loads(Child, y) == Child('toto', None)
+
+
+def test_mangled_attributes():
+    @pyckson.pyckson
+    @dataclass
+    class Parent:
+        mandatory_one: str
+        mandatory_two: str
+
+    @pyckson.pyckson
+    @dataclass
+    class Child(Parent):
+        mandatory_two: Optional[str]
+
+    assert getattr(Parent, '__Parent_pyckson') is True
+    assert getattr(Child, '__Child_pyckson') is True
+    assert not hasattr(Parent, '__Child_pyckson')
+    assert hasattr(Child, '__Parent_pyckson')
