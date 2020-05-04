2b89c4b5737f90c80922536306743a9fcc7e81d2
Jean Giard <1167353+JGiard@users.noreply.github.com>
2020-05-04 11:03:02 +0200

    Prepare for v1.9.0

--- src/pyckson/__init__.py
+++ src/pyckson/__init__.py
@@ -7,4 +7,4 @@ from pyckson.serializers.base import Serializer
 from pyckson.dates.helpers import configure_date_formatter, configure_explicit_nulls
 from pyckson.defaults import set_defaults
 
-__version__ = '1.8.1'
+__version__ = '1.9.0'
