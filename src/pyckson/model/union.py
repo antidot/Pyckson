831c810b96c0ed1b6c254b0b6b2c3b1b259b51bb
Jean Giard <1167353+JGiard@users.noreply.github.com>
2020-11-18 15:51:18 +0100

    Fix optional inspection in 3.9

--- src/pyckson/model/union.py
+++ src/pyckson/model/union.py
@@ -4,7 +4,7 @@ from typing import Tuple, Union
 def inspect_optional_typing(annotation) -> Tuple[bool, type]:
     # seems like at some point internal behavior on typing Union changed
     # https://bugs.launchpad.net/ubuntu/+source/python3.5/+bug/1650202
-    if "Union" not in str(annotation):
+    if "Union" not in str(annotation) and "Optional" not in str(annotation):
         return False, type(None)
 
     if hasattr(annotation, '__origin__'):
