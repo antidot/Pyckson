661811d564fb7d062d1f6f34ea41a954709a7612
Jean Giard <1167353+JGiard@users.noreply.github.com>
2023-05-03 16:02:17 +0200

    Add suport for generic list typing

---
+++ tests/test_list_typing.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from pyckson import parse, serialize
+
+
+def test_list_typing():
+    @dataclass
+    class Bar:
+        y: int
+
+    @dataclass
+    class Foo:
+        x: list[Bar]
+
+    assert parse(Foo, {'x': [{'y': 2}, {'y': 3}]}) == Foo([Bar(2), Bar(3)])
+    assert serialize(Foo([Bar(2), Bar(3)])) == {'x': [{'y': 2}, {'y': 3}]}
