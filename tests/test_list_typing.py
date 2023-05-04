ee7d111d2a0bc51f044cd4eaf3493b4b6fb98801
Jean Giard <1167353+JGiard@users.noreply.github.com>
2023-05-04 10:03:27 +0200

    Add generic dict typing

--- tests/test_list_typing.py
+++ tests/test_list_typing.py
@@ -27,3 +27,16 @@ def test_set_typing():
 
     assert parse(Foo, {'x': [{'y': 2}, {'y': 3}]}) == Foo({Bar(2), Bar(3)})
     assert serialize(Foo({Bar(2), Bar(3)})) == {'x': [{'y': 2}, {'y': 3}]}
+
+
+def test_dict_typing():
+    @dataclass
+    class Bar:
+        y: int
+
+    @dataclass
+    class Foo:
+        x: dict[str, Bar]
+
+    assert parse(Foo, {'x': {'a': {'y': 2}, 'b': {'y': 3}}}) == Foo({'a': Bar(2), 'b': Bar(3)})
+    assert serialize(Foo({'a': Bar(2), 'b': Bar(3)})) == {'x': {'a': {'y': 2}, 'b': {'y': 3}}}
